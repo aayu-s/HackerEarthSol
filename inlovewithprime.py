caa6ca3734f02813ab4ceb6f6cf58ccb229401c4
Ayushi <ayuships33@gmail.com>
2019-03-20 00:20:10 +0530

    New logic

--- inlovewithprime.py
+++ inlovewithprime.py
@@ -1,35 +1,47 @@
 
-import math
 
+#new changes
+# TROLLED : Just realised every number can be represented as prime number
 t = int(input())
-inp = []
-out = []
 while t>0:
-    n = int(input())
-    inp.append(n)
-    t-=1
+    t -= 1
+    a = int(input())
+    if a != 2:
+        print("Deepa")
+    else:
+        print("Arjit")
+
+# import math
+
+# t = int(input())
+# inp = []
+# out = []
+# while t>0:
+#     n = int(input())
+#     inp.append(n)
+#     t-=1
     
 
-def isPrime(n): 
-    if n <= 1: 
-        return False
-    for i in range(2, (int)(math.sqrt(n))): 
-        if n % i == 0: 
-            return False
-    return True
+# def isPrime(n): 
+#     if n <= 1: 
+#         return False
+#     for i in range(2, (int)(math.sqrt(n))): 
+#         if n % i == 0: 
+#             return False
+#     return True
   
-def isPossible(n):
-    for i in range(2, n-1):
-        print(n-i,i)
-        if isPrime(n-i) and isPrime(i):
-            return True
+# def isPossible(n):
+#     for i in range(2, n-1):
+#         print(n-i,i)
+#         if isPrime(n-i) and isPrime(i):
+#             return True
         
-for elem in inp:
-    if elem == 1:
-        out.append("Arjit")
-    elif isPossible(elem) == True: 
-        out.append("Deepa") 
-    else: 
-        out.append("Arjit")
+# for elem in inp:
+#     if elem == 1:
+#         out.append("Arjit")
+#     elif isPossible(elem) == True: 
+#         out.append("Deepa") 
+#     else: 
+#         out.append("Arjit")
         
-print(*out, sep="\n")
+# print(*out, sep="\n")
